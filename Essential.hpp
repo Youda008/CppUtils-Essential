f824c8cece46b3a5ad2e522276afa66b8b550af7
Youda <youda008@seznam.cz>
2020-11-16 22:07:44 +0100

    repository reorganization

---
+++ Essential.hpp
@@ -0,0 +1,29 @@
+//======================================================================================================================
+// Project: CppUtils
+//----------------------------------------------------------------------------------------------------------------------
+// Author:      Jan Broz (Youda008)
+// Created on:  1.11.2020
+// Description: essential includes and declarations
+//======================================================================================================================
+
+#ifndef CPPUTILS_ESSENTIAL_INCLUDED
+#define CPPUTILS_ESSENTIAL_INCLUDED
+
+
+#include <memory>
+#include <utility>
+#include <optional>
+#include <cstdint>
+#include <climits>
+#include <type_traits>
+
+using uint = unsigned int;
+using ushort = unsigned short;
+using ulong = unsigned long;
+using ullong = unsigned long long;
+//using byte = uint8_t;
+
+using std::move;
+
+
+#endif // CPPUTILS_ESSENTIAL_INCLUDED
