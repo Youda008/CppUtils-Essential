93acb0d0d6359a4bd7dfbd24459444d1a34f8240
Youda <youda008@gmail.com>
2021-07-13 23:03:54 +0200

    improvements to span

--- Span.hpp
+++ Span.hpp
@@ -17,64 +17,6 @@ namespace own {
 
 
 //======================================================================================================================
-/// Generalization over continuous-memory containers.
-/** To be used instead of raw buffer pointers as often as possible. */
-
-template< typename ElemType >
-class span
-{
-	ElemType * _begin;
-	ElemType * _end;
-
-	using thisSpan = span< ElemType >;
-
- public:
-
-	span() noexcept : _begin( nullptr ), _end( nullptr ) {}
-
-	// the templates for other type are required to allow constructing  span< const char > from span< char >
-
-	// construct manually from pair of pointers
-	template< typename OtherType,
-		typename std::enable_if<std::is_convertible< OtherType*, ElemType* >::value, int >::type = 0 >
-	span( OtherType * begin, ElemType * end ) noexcept : _begin( begin ), _end( end ) {}
-
-	// construct manually from a data pointer and size
-	template< typename OtherType,
-		typename std::enable_if< std::is_convertible< OtherType*, ElemType* >::value, int >::type = 0 >
-	span( OtherType * data, size_t size ) noexcept : _begin( data ), _end( data + size ) {}
-
-	// deduce from static C array
-	template< typename OtherType, size_t Size,
-		typename std::enable_if< std::is_convertible< OtherType*, ElemType* >::value, int >::type = 0 >
-	span( OtherType (& arr) [Size] ) noexcept : thisSpan( arr, Size ) {}
-
-	// copy
-	template< typename OtherType,
-		typename std::enable_if< std::is_convertible< OtherType*, ElemType* >::value, int >::type = 0 >
-	span( const span< OtherType > & other ) noexcept : thisSpan( other.begin(), other.end() ) {}
-
-	// assign
-	template< typename OtherType,
-		typename std::enable_if< std::is_convertible< OtherType*, ElemType* >::value, int >::type = 0 >
-	span< ElemType > & operator=( const span< OtherType > & other ) noexcept
-		{ _begin = other.begin(); _end = other.end(); return *this; }
-
-	ElemType * begin() const noexcept { return _begin; }
-	ElemType * end() const noexcept { return _end; }
-	ElemType * data() const noexcept { return _begin; }
-	size_t size() const noexcept { return size_t( _end - _begin ); }
-	bool empty() const noexcept { return _begin == _end; }
-
-	template< typename OtherType >
-	span< OtherType > cast() const noexcept
-	{
-		return { reinterpret_cast< OtherType * >( _begin ), reinterpret_cast< OtherType * >( _end ) };
-	}
-};
-
-
-//======================================================================================================================
 /// Variant of span with compile-time length.
 /** Use this when you know already know how long the span will be when writing your code. */
 
@@ -118,6 +60,70 @@ class fixed_span
 
 
 //======================================================================================================================
+/// Generalization over continuous-memory containers.
+/** To be used instead of raw buffer pointers as often as possible. */
+
+template< typename ElemType >
+class span
+{
+	ElemType * _begin;
+	ElemType * _end;
+
+	using thisSpan = span< ElemType >;
+
+ public:
+
+	span() noexcept : _begin( nullptr ), _end( nullptr ) {}
+
+	// the templates with OtherType are required to allow constructing  span< const char > from span< char >
+
+	// construct manually from pair of pointers
+	template< typename OtherType,
+		typename std::enable_if<std::is_convertible< OtherType*, ElemType* >::value, int >::type = 0 >
+	span( OtherType * begin, ElemType * end ) noexcept : _begin( begin ), _end( end ) {}
+
+	// construct manually from a data pointer and size
+	template< typename OtherType,
+		typename std::enable_if< std::is_convertible< OtherType*, ElemType* >::value, int >::type = 0 >
+	span( OtherType * data, size_t size ) noexcept : _begin( data ), _end( data + size ) {}
+
+	// construct from fixed_span
+	template< typename OtherType, size_t Size,
+		typename std::enable_if< std::is_convertible< OtherType*, ElemType* >::value, int >::type = 0 >
+	span( const fixed_span< OtherType, Size > & other ) noexcept : thisSpan( other.data, Size ) {}
+
+	// deduce from static C array
+	template< typename OtherType, size_t Size,
+		typename std::enable_if< std::is_convertible< OtherType*, ElemType* >::value, int >::type = 0 >
+	span( OtherType (& arr) [Size] ) noexcept : thisSpan( arr, Size ) {}
+
+	// copy
+	template< typename OtherType,
+		typename std::enable_if< std::is_convertible< OtherType*, ElemType* >::value, int >::type = 0 >
+	span( const span< OtherType > & other ) noexcept : thisSpan( other.begin(), other.end() ) {}
+
+	// assign
+	template< typename OtherType,
+		typename std::enable_if< std::is_convertible< OtherType*, ElemType* >::value, int >::type = 0 >
+	span< ElemType > & operator=( const span< OtherType > & other ) noexcept
+		{ _begin = other.begin(); _end = other.end(); return *this; }
+
+	ElemType * begin() const noexcept { return _begin; }
+	ElemType * end() const noexcept { return _end; }
+	ElemType * data() const noexcept { return _begin; }
+	size_t size() const noexcept { return size_t( _end - _begin ); }
+	bool empty() const noexcept { return _begin == _end; }
+
+	template< typename OtherType >
+	span< OtherType > cast() const noexcept
+	{
+		return { reinterpret_cast< OtherType * >( _begin ), reinterpret_cast< OtherType * >( _end ) };
+	}
+};
+
+
+
+//======================================================================================================================
 //  specialized spans
 
 class byte_span : public span< uint8_t >
@@ -126,17 +132,12 @@ class byte_span : public span< uint8_t >
 
  public:
 
-	using baseSpan::span;
+	using span< uint8_t >::span;
 
 	byte_span( const baseSpan & other ) noexcept : baseSpan( other ) {}
 	byte_span & operator=( const baseSpan & other ) noexcept { baseSpan::operator=( other ); return *this; }
 
-	// allow using chars where bytes are expected
-	byte_span( const span< char > & other ) noexcept
-		: baseSpan( reinterpret_cast< uint8_t * >( other.data() ), other.size() ) {}
-	template< size_t Size >
-	byte_span( char (& arr) [Size] ) noexcept
-		: baseSpan( reinterpret_cast< uint8_t * >( arr ), Size ) {}
+	span< char > toChars() const noexcept { return { reinterpret_cast< char * >( data() ), size() }; }
 };
 
 class const_byte_span : public span< const uint8_t >
@@ -145,23 +146,12 @@ class const_byte_span : public span< const uint8_t >
 
  public:
 
-	using baseSpan::span;
+	using span< const uint8_t >::span;
 
 	const_byte_span( const baseSpan & other ) noexcept : baseSpan( other ) {}
 	const_byte_span & operator=( const baseSpan & other ) noexcept { baseSpan::operator=( other ); return *this; }
 
-	// allow using chars where bytes are expected
-	// TODO: simpler
-	const_byte_span( const span< const char > & other ) noexcept
-		: baseSpan( reinterpret_cast< const uint8_t * >( other.data() ), other.size() ) {}
-	const_byte_span( const span< char > & other ) noexcept
-		: baseSpan( reinterpret_cast< uint8_t * >( other.data() ), other.size() ) {}
-	template< size_t Size >
-	const_byte_span( const char (& arr) [Size] ) noexcept
-		: baseSpan( reinterpret_cast< const uint8_t * >( arr ), Size ) {}
-	template< size_t Size >
-	const_byte_span( char (& arr) [Size] ) noexcept
-		: baseSpan( reinterpret_cast< uint8_t * >( arr ), Size ) {}
+	span< const char > toChars() const noexcept { return { reinterpret_cast< const char * >( data() ), size() }; }
 };
 
 class char_span : public span< char >
@@ -170,17 +160,12 @@ class char_span : public span< char >
 
  public:
 
-	using baseSpan::span;
+	using span< char >::span;
 
 	char_span( const baseSpan & other ) noexcept : baseSpan( other ) {}
 	char_span & operator=( const baseSpan & other ) noexcept { baseSpan::operator=( other ); return *this; }
 
-	// allow using bytes where chars are expected
-	char_span( const span< uint8_t > & other ) noexcept
-		: baseSpan( reinterpret_cast< char * >( other.data() ), other.size() ) {}
-	template< size_t Size >
-	char_span( const uint8_t (& arr) [Size] ) noexcept
-		: baseSpan( reinterpret_cast< char * >( arr ), Size ) {}
+	span< uint8_t > toBytes() const noexcept { return { reinterpret_cast< uint8_t * >( data() ), size() }; }
 };
 
 class const_char_span : public span< const char >
@@ -189,36 +174,76 @@ class const_char_span : public span< const char >
 
  public:
 
-	using baseSpan::span;
+	using span< const char >::span;
 
 	const_char_span( const baseSpan & other ) noexcept : baseSpan( other ) {}
 	const_char_span & operator=( const baseSpan & other ) noexcept { baseSpan::operator=( other ); return *this; }
 
-	// allow using bytes where chars are expected
-	// TODO: simpler
-	const_char_span( const span< const uint8_t > & other ) noexcept
-		: baseSpan( reinterpret_cast< const char * >( other.data() ), other.size() ) {}
-	const_char_span( const span< uint8_t > & other ) noexcept
-		: baseSpan( reinterpret_cast< char * >( other.data() ), other.size() ) {}
-	template< size_t Size >
-	const_char_span( const uint8_t (& arr) [Size] ) noexcept
-		: baseSpan( reinterpret_cast< const char * >( arr ), Size ) {}
-	template< size_t Size >
-	const_char_span( uint8_t (& arr) [Size] ) noexcept
-		: baseSpan( reinterpret_cast< char * >( arr ), Size ) {}
+	span< const uint8_t > toBytes() const noexcept { return { reinterpret_cast< const uint8_t * >( data() ), size() }; }
+};
+
+
+
+
+template< size_t Size >
+class fixed_byte_span : public fixed_span< uint8_t, Size >
+{
+	using baseSpan = fixed_span< uint8_t, Size >;
+
+ public:
+
+	using fixed_span< uint8_t, Size >::fixed_span;
+
+	fixed_byte_span( const baseSpan & other ) noexcept : baseSpan( other ) {}
+	fixed_byte_span & operator=( const baseSpan & other ) noexcept { baseSpan::operator=( other ); return *this; }
+
+	fixed_span< char, Size > toChars() const noexcept { return { reinterpret_cast< char * >( this->data() ) }; }
 };
 
 template< size_t Size >
-using fixed_byte_span = fixed_span< uint8_t, Size >;
+class fixed_const_byte_span : public fixed_span< const uint8_t, Size >
+{
+	using baseSpan = fixed_span< const uint8_t, Size >;
+
+ public:
+
+	using fixed_span< const uint8_t, Size >::fixed_span;
+
+	fixed_const_byte_span( const baseSpan & other ) noexcept : baseSpan( other ) {}
+	fixed_const_byte_span & operator=( const baseSpan & other ) noexcept { baseSpan::operator=( other ); return *this; }
+
+	fixed_span< const char, Size > toChars() const noexcept { return { reinterpret_cast< const char * >( this->data() ) }; }
+};
 
 template< size_t Size >
-using fixed_const_byte_span = fixed_span< const uint8_t, Size >;
+class fixed_char_span : public fixed_span< char, Size >
+{
+	using baseSpan = fixed_span< char, Size >;
+
+ public:
+
+	using fixed_span< char, Size >::fixed_span;
+
+	fixed_char_span( const baseSpan & other ) noexcept : baseSpan( other ) {}
+	fixed_char_span & operator=( const baseSpan & other ) noexcept { baseSpan::operator=( other ); return *this; }
+
+	fixed_span< uint8_t, Size > toBytes() const noexcept { return { reinterpret_cast< uint8_t * >( this->data() ) }; }
+};
 
 template< size_t Size >
-using fixed_char_span = fixed_span< char, Size >;
+class fixed_const_char_span : public fixed_span< const char, Size >
+{
+	using baseSpan = fixed_span< const char, Size >;
 
-template< size_t Size >
-using fixed_char_byte_span = fixed_span< const char, Size >;
+ public:
+
+	using fixed_span< const char, Size >::fixed_span;
+
+	fixed_const_char_span( const baseSpan & other ) noexcept : baseSpan( other ) {}
+	fixed_const_char_span & operator=( const baseSpan & other ) noexcept { baseSpan::operator=( other ); return *this; }
+
+	fixed_span< const uint8_t, Size > toBytes() const noexcept { return { reinterpret_cast< const uint8_t * >( this->data() ) }; }
+};
 
 
 //======================================================================================================================
